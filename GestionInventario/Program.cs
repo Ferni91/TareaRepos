924a3b5011c1455be575863d68f6c8bb4d8a59b3
carlosferni090@gmail.com <carlosferni09@gmail.com>
2024-11-08 18:14:54 -0600

    Declaracion de la clase program y metodo de ingreso de productos por el usuario

--- GestionInventario/Program.cs
+++ GestionInventario/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+namespace GestiónInventario
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Inventario inventario = new Inventario();
+            Console.WriteLine("Bienvenido al sistema de gestión de inventario");
+
+            // Ingreso de productos por el usuario.
+            Console.WriteLine("¿Cuántos productos desea ingresar?");
+            int cantidad;
+            while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+            {
+                Console.WriteLine("Por favor, ingrese un número entero positivo.");
+            }
+        }
+    }
+}
